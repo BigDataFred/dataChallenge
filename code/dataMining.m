620e3a882c72e1dc350fead648562b49a4f3bf8e
Fred <fredericrouxw@gmail.com>
2018-03-23 16:53:38 +0000

    added autocorrelation of event types and histogram of timestamps

--- code/dataMining.m
+++ code/dataMining.m
@@ -1,5 +1,5 @@
 %%
-p2d = '/home/adf/rouxf/Downloads/data-lover-challenge/';
+p2d = '/home/adf/rouxf/Downloads/data-lover-challenge/csvDat/';
 files = dir([p2d,'*.csv'])
 
 %%
@@ -51,21 +51,45 @@ tp = X(:,end);
 
 %%
 tpID = unique(tp);
-lab = zeros(1,length(tp));
+lab = zeros(length(tp),1);
 for it = 1:length(tpID)
     lab(find(strcmp(tp,tpID(it)))) = it;
 end;
+M(:,end+1) = lab;
 
+seg = [1;find([0;diff(M(:,2))]==1);size(M,1)];
+
+%%
 [n1,x1] = hist(lab,[1:length(tpID)]);
 [n2,x2] = hist(M(:,2),sort(unique(M(:,2))));
 figure;
-subplot(121);
+subplot(221);
 bar(x1,n1);
 ylabel('count');
 set(gca,'XTick',1:length(tpID));
 set(gca,'XTickLabel',tp);
-subplot(122);
+subplot(222);
+hold on;
+%[ax,h1,h2] = plotyy(1:size(M,1),M(:,2),1:size(M,1),[0;diff(M(:,2))]);
+for it = 1:length(seg)-1
+    h = area([seg(it) seg(it+1)-1],[max(M(:,2)) max(M(:,2))],min(M(:,2)));
+    if mod(it,2) == 0
+        set(h,'FaceColor',[1 1 1],'EdgeColor',[1 1 1]);
+    else
+        set(h,'FaceColor',[.75 .75 .75],'EdgeColor',[.25 .25 .25]);
+    end;
+end;
+plot(1:size(M,1),M(:,2),'y');    
+ylabel(hdr{2});
+axis('tight');
+subplot(223);
+[xc,lag] = xcorr(M(:,end)-mean(M(:,end)),500,'coeff');
+plot(lag,xc);
+axis tight;
+subplot(224);
+hold on;
 bar(x2,n2);
+plot([x2(1) x2(end)],[100 100],'r--');
 set(gca,'XTick',sort(unique(M(1:1000:size(M,1),2))));
 set(gca,'XTickLabel',sort(unique(M(1:1000:size(M,1),2))));
 xlabel(hdr{2});
